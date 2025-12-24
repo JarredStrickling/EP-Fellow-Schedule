14e422c92b9e3da48dedb73892583282ad59f737
Jarred Strickling <jarredstrickling@gmail.com>
2025-12-23 19:54:39 -0500

    Fix build: remove missing CreateSchedule import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CreateSchedule from "./CreateSchedule.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CreateSchedule />
   </React.StrictMode>
 );
